2ad33d7ab584f0ce08568859ddec61c05abd6910
Igor <id17002@my.bristol.ac.uk>
2018-10-15 18:20:41 +0100

    First question solved

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,15 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		int result = 0;
+		for(int i = 0; i < portfolios.length; i++) {
+			for(int j = i+1; j < portfolios.length; j++) {
+				if( (portfolios[i]^portfolios[j]) > result ) {
+					result = (portfolios[i]^portfolios[j]);
+				}
+			}
+		}
+		return result;
 	}
 
 }
