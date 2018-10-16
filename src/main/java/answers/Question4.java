d9db30c04027f02cf2b6ab56dd2b5c7f52d707bd
Igor <id17002@my.bristol.ac.uk>
2018-10-16 17:40:49 +0100

    Q4 first try

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -3,8 +3,29 @@ package answers;
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
+		int bigNumber = 10000007;
+		int result = bigNumber;
+		int[][] arr = new int[rows.length][rows[0].length];
+		for(int i = 0; i < rows.length; i++) {
+			for(int j = 0; j < rows[i].length; j++) {
+				if(rows[i][j].equals("X")) arr[i][j] = bigNumber;
+				else arr[i][j] = Integer.parseInt(rows[i][j]);
+			}
+		}
+
+		for(int i = 0; i < arr.length; i++) {
+			int sum = 0;
+			for(int j = 0; j < numberMachines; j++) {
+				sum += arr[i][j];
+			}
+			if(sum < result) result = sum;
+			for(int j = numberMachines; j < arr[i].length; j++) {
+				sum -= arr[i][j-numberMachines];
+				sum += arr[i][j];
+				if(sum < result) result = sum;
+			}
+		}
+		return result;
 	}
 
 }
