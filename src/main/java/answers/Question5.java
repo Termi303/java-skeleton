ff9eae57a7b2f0f3801f09404892c03d5e0c178a
Igor <id17002@my.bristol.ac.uk>
2018-10-17 21:14:03 +0100

    Q5 first attempt

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,10 +1,22 @@
 package answers;
+import java.util.*;
 
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		// TODO Auto-generated method stub
-		return -1;
+		int[] result = new int[totalValue+1];
+		//Arrays.sort(allowedAllocations);
+		for(int i = 1; i <= totalValue; i++) {
+			result[i] = 1000000007;
+		}
+		for(int val : allowedAllocations) {
+			for(int i = 0; i+val <= totalValue; i++) {
+				if(result[i]+1 < result[i+val]) {
+					result[i+val] = result[i]+1;
+				}
+			}
+		}
+		return result[totalValue];
 	}
 
 }
