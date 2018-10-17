a2028f76ef8298ac61249a006fc7a4ddbccc879a
Igor <id17002@my.bristol.ac.uk>
2018-10-17 19:41:08 +0100

    Q4 - long

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -3,23 +3,23 @@ package answers;
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		int bigNumber = 1000000007;
-		int result = bigNumber;
-		int[][] arr = new int[rows.length][rows[0].length];
+		long bigNumber = 1000000000000000007L;
+		long result = bigNumber;
+		long[][] arr = new long[rows.length][rows[0].length];
 		for(int i = 0; i < rows.length; i++) {
 			for(int j = 0; j < rows[i].length; j++) {
-				if(rows[i][j].equals("X")) arr[i][j] = bigNumber;
-				else arr[i][j] = Integer.parseInt(rows[i][j]);
+				if(rows[i][j].equals("X")) arr[i][j] = -1L;
+				else arr[i][j] = (long)(Integer.parseInt(rows[i][j]));
 			}
 		}
 		for(int i = 0; i < arr.length; i++) {
 			int start = 0, end = 0;
-			int sum = 0;
+			long sum = 0L;
 			while(end < arr[i].length) {
-				if(rows[i][end].equals("X")) {
+				if(arr[i][end] == -1L) {
 					end++;
 					start = end;
-					sum = 0;
+					sum = 0L;
 					continue;
 				} else {
 					if(end-start == numberMachines) {
@@ -33,7 +33,7 @@ public class Question4 {
 				}
 			}
 		}
-		return result;
+		return (int)result;
 	}
 
 }
