d06d0248918ec5623a63abfa23eb62c580d0099e
Igor <id17002@my.bristol.ac.uk>
2018-10-18 17:59:25 +0100

    Q5 sort try

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -5,11 +5,13 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		int[] result = new int[totalValue+1];
-		//Arrays.sort(allowedAllocations);
+		Arrays.sort(allowedAllocations);
 		for(int i = 1; i <= totalValue; i++) {
-			result[i] = 1000000007;
+			result[i] = 2147483647;
 		}
-		for(int val : allowedAllocations) {
+		int val = 0;
+		for(int j = allowedAllocations.length-1; j >= 0; j--) {
+			val = allowedAllocations[j];
 			for(int i = 0; i+val <= totalValue; i++) {
 				if(result[i]+1 < result[i+val]) {
 					result[i+val] = result[i]+1;
