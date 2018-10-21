aed41ec98c30532a9c6bee654211f734865430c7
Igor <id17002@my.bristol.ac.uk>
2018-10-21 21:38:35 +0100

    Q6 wait with optimization

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -22,6 +22,35 @@ public class Question6 {
 	}
 
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
+		return bruteforce(numServers, targetServer, times);
+	}
+
+	public static int bruteforce(int numServers, int targetServer, int[][] times) {
+		int[] minDistance = new int[numServers];
+		boolean[] visited = new boolean[numServers];
+		for(int i = 0; i < numServers; i++) {
+			minDistance[i] = 2147483647;
+		}
+
+		PriorityQueue<Pair> S = new PriorityQueue<>();
+		S.add(new Pair(0, 0));
+		while(S.size() > 0 && !visited[targetServer]) {
+			Pair p = S.poll();
+			if(visited[p.where]) continue;
+			visited[p.where] = true;
+			minDistance[p.where] = p.dist;
+			if(p.where == targetServer) break;
+			for(int i = 0; i < numServers; i++) {
+				if(!visited[i] && p.dist + times[p.where][i] < minDistance[i]) {
+					minDistance[i] = p.dist + times[p.where][i];
+					S.add(new Pair(i, minDistance[i]));
+				}
+			}
+		}
+		return minDistance[targetServer];
+	}
+
+	public static int optimal(int numServers, int targetServer, int[][] times) {
 		int[] minDistance = new int[numServers];
 		boolean[] visited = new boolean[numServers];
 		List<Integer> pos = new LinkedList<>();
