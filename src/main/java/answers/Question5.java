b111c386aa3b9061065027d07c69bc94346d6957
Igor <id17002@my.bristol.ac.uk>
2018-10-22 12:38:52 +0100

    Q5 fix2

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -5,7 +5,7 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		int[] result = new int[totalValue+1];
-		int bigNum = 2147483647;
+		int bigNum = 1000000007;
 		//Arrays.sort(allowedAllocations);
 		for(int i = 1; i <= totalValue; i++) {
 			result[i] = bigNum;
@@ -17,7 +17,8 @@ public class Question5 {
 				}
 			}
 		}
-		return (result[totalValue] == bigNum) ? 0 : result[totalValue];
+		if(result[totalValue] == bigNum) return 0;
+		return result[totalValue];
 	}
 
 }
